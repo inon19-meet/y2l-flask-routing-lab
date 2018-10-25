1116fba656b67f404b5568f27758f4204c4e2642
= <=>
2018-10-25 18:50:39 +0300

    j

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+    return render_template("home.html")
+
+
+@app.route('/shop')
+def shop():
+	oranges = ["orange", "blue orange", "orange with smiley"]
+	return render_template(
+		"shop.html",
+		oranges=oranges,
+		no_oranges=False)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
